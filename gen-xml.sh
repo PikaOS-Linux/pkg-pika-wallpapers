43e5689be3d5ff3323fbc53fa4950864de1228bd
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-28 18:57:30 +0300

    update

---
+++ gen-xml.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE wallpapers SYSTEM \"gnome-wp-list.dtd\">\n<wallpapers>" > pika-wallpapers.xml
+for i in $(ls backgrounds/pika/)
+do
+echo -e "\t<wallpaper>\n\t\t<name>$i</name>\n\t\t<filename>/usr/share/backgrounds/pika/$i</filename>\n\t\t<options>zoom</options>\n\t\t<pcolor>#000000</pcolor>\n\t\t<scolor>#000000</scolor>\n\t\t<shade_type>solid</shade_type>\n\t</wallpaper>" >> pika-wallpapers.xml
+done
+echo -e "</wallpapers>" >> pika-wallpapers.xml
--- main.sh
+++ main.sh
@@ -8,12 +8,6 @@ add-apt-repository https://ppa.pika-os.com
 add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
-echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE wallpapers SYSTEM \"gnome-wp-list.dtd\">\n<wallpapers>" > pika-wallpapers.xml
-for i in $(ls wallpapers)
-do
-echo -e "\t<wallpaper>\n\t\t<name>$i</name>\n\t\t<filename>/usr/share/backgrounds/pika/$i</filename>\n\t\t<options>zoom</options>\n\t\t<pcolor>#000000</pcolor>\n\t\t<scolor>#000000</scolor>\n\t\t<shade_type>solid</shade_type>\n\t</wallpaper>" >> pika-wallpapers.xml
-done
-echo -e "</wallpapers>" >> pika-wallpapers.xml
 mkdir -p ./pika-wallpapers
 cp -rvf ./* ./pika-wallpapers || echo
 cd ./pika-wallpapers
