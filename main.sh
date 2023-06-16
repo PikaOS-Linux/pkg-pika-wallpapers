6c236c10bc854e2e7118e894614cdb17d5089b0b
ferrreo <harderthanfire@gmail.com>
2023-06-16 18:59:14 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,11 +2,6 @@
 
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 ./gen-xml.sh
 mkdir -p ./pika-wallpapers
@@ -14,8 +9,6 @@ cp -rvf ./* ./pika-wallpapers || echo
 cd ./pika-wallpapers
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
